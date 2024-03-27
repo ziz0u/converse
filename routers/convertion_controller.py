718f5fac29e8fcb2d1a9cd030c750911a0c730b5
ziz0u <zizou.tol@gmail.com>
2024-03-27 10:48:29 -0600

    Uploading folders...
    ...

---
+++ routers/convertion_controller.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+from convertion import Convertion
+from fastapi import APIRouter, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+router = APIRouter(
+    prefix="/convertion",
+    responses={404: {"description": "Página no encontrada"}},
+)
+
+convertion = Convertion()
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/", response_class=HTMLResponse)
+def show(request: Request):
+    """
+    Conviente el recurso csv a un formato html
+    """
+
+    # return templates.TemplateResponse(
+    #     request=request, name="convertion.html", context={"csvlist": convertion.to_list()}
+    # )
+
+    template = 'convertion.html'
+    context = {'request': request, "csvlist": convertion.to_list()}
+
+    return templates.TemplateResponse(template, context)

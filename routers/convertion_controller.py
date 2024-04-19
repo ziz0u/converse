2ba9446404f3e23212beb4c6f2a3c44754829620
oscarf <urielwel@gmail.com>
2024-04-19 01:21:55 -0600

    Refactiracion de parámatros

--- routers/convertion_controller.py
+++ routers/convertion_controller.py
@@ -20,7 +20,8 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/", response_class=HTMLResponse)
-def show(request: Request, query: str = "", message_error: str = ""):
+def show(request: Request, isSpecificDay : bool = False, day : str = "day",
+        start_hour : str = "", end_hour : str = "", servers : str = "", message_error: str = ""):
     """
     Conviente el recurso csv a un formato html
     """
@@ -28,83 +29,53 @@ def show(request: Request, query: str = "", message_error: str = ""):
     return templates.TemplateResponse('convertion.html', {
         'request': request, 
         "csvlist": convertion.to_list(),
-        "query": query,
+        "isSpecificDay": isSpecificDay,
+        "day": day,
+        "start_hour": start_hour,
+        "end_hour": end_hour,
+        "servers": servers,
         "message_error": message_error
     })
 
 
 @router.get("/analyze", response_class=HTMLResponse)
-def analize_with_script_bash(request: Request, query: str):
+def analize_with_script_bash(request: Request, isSpecificDay : bool = False, day : str = "day",
+        start_hour : str = "", end_hour : str = "", servers : str = ""):
     """
     Este wey recaba parámetros de fecha hora y/o nombre del servidor, los params
     son usados como parámetros del script bash (ansible-playbook).
 
-    El formato de una consulta puede ser:
-
-    server1
-    servers=server1,server2
-    time=HH:mm HH:mm
-    year=dd
-
-    O convinacion de las anteriores separadas por ";", por ejemplo:
-    servers=server1,server2; day=11; time=00:00 23:00
-    server2; day=01
-    servers=server1,server2,s3; time=00:00        
-
-
     @return #this.#show
     """
+    servers = servers.strip()
 
-    query = query.strip()
+    if servers == "":
+        return show(request, isSpecificDay, day, start_hour, end_hour, servers)
 
-    if query == "": # Caso de uso no hay cadena -> optiene los servers por default
-        return show(request)
+    if not isSpecificDay or day == "day":
+        day = date.today().strftime('%d')
 
-    hours = ["00:00:00", "23:59:59"]
-    day = date.today().strftime('%d')
-    servers = None
-  
-    for predicate in re.split(";", query):
-        predicate = predicate.strip()
+    this_month = date.today().strftime('%m')
+    this_year = date.today().strftime('%Y')
 
-        if re.search('^ ?\\w+$', predicate):
-            servers = [predicate]
-            continue 
+    try:
+        time.strptime(f"{day}/{this_month}/{this_year}", '%d/%m/%Y')
+    except ValueError:
+        return show(request, isSpecificDay, day, start_hour, end_hour, servers, 
+                f"Día del mes no válido ( {day} )")
 
-        if  re.search('^ ?servers=\\w+(, ?\\w+)*$', predicate):
-            servers = re.split(",", re.split("=", predicate)[1])
-            continue
-            
-        if  re.search('^ ?time=\\d+:\\d+ \\d+:\\d+$', predicate):
-            hours = re.split(" ", re.split("=", predicate)[1])
 
-            try:
-                time.strptime(hours[0], '%H:%M')
-                time.strptime(hours[1], '%H:%M')
-            except ValueError:
-                return show(request, query, f"Formato de HORAS no válido ( {predicate} )")
+    hours = [f"{start_hour}:00", f"{end_hour}:00"]
 
-            continue
+    if start_hour == "" and end_hour == "":
+        hours = ["00:00:00", "23:59:59"]
 
-        if  re.search('^ ?day=\\d+$', predicate):
-            day = re.split("=", predicate)[1]
-            this_month = date.today().strftime('%m')
-            this_year = date.today().strftime('%Y')
-
-            try:
-                time.strptime(f"{day}/{this_month}/{this_year}", '%d/%m/%Y')
-            except ValueError:
-                return show(request, query, f"Día del mes no válido ( {predicate} )")
-
-            hours[0] = f"{hours[0]}:00"
-            hours[1] = f"{hours[1]}:00"
-
-            continue
-
-        return show(request, query, f"Revisa el predicado '{predicate}'")
-
-    if servers is None:
-        return show(request, query, "Debes de agregar al menos el nombre de un server (servers=<s1>,<s2>,<sN>)")
+    try:
+        time.strptime(hours[0], '%H:%M:%S')
+        time.strptime(hours[1], '%H:%M:%S')
+    except ValueError:
+        return show(request, isSpecificDay, day, start_hour, end_hour, servers, 
+                f"Formato de HORAS no válido ( {start_hour} - {end_hour} )")
 
     os.system(f"""
         ansible-playbook -u user -s \\
@@ -113,5 +84,5 @@ def analize_with_script_bash(request: Request, query: str):
     """)
 
 
-    return show(request, query)
+    return show(request, isSpecificDay, day, start_hour, end_hour, servers)
 

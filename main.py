a5eda9582d98e8143d5c32b239f71fe533ba43dc
ziz0u <zizou.tol@gmail.com>
2024-04-22 01:24:11 -0600

    Added date in log

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import re
 import sys, os
 import uvicorn
+from uvicorn.config import LOGGING_CONFIG
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routers import convertion_controller
@@ -30,5 +31,6 @@ app.include_router(convertion_controller.router)
 
 if __name__ == "__main__":
     # uvicorn.run(app, host = "0.0.0.0", port=5000, log_level="info", ssl_keyfile = "certificates/private.key", ssl_certfile = "certificates/certchain.pem")
+    LOGGING_CONFIG["formatters"]["access"]["fmt"] = "%(asctime)s %(levelprefix)s %(client_addr)s - \"%(request_line)s\" %(status_code)s"
+    LOGGING_CONFIG["formatters"]["access"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
     uvicorn.run(app, host = "0.0.0.0", port=5000, log_level="info")
-
